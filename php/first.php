eefb6d7f42f3cd988724377f58b8a4349e938653
alejavaz <59677187+alejavaz@users.noreply.github.com>
2020-02-19 22:05:03 -0600

    Learning PHP lesson 1

---
+++ php/first.php
@@ -0,0 +1,59 @@
+<?php
+
+// comentarios
+
+/* multiples 
+lineas */
+
+//variables
+
+//underscore
+$first_name = "Alex";
+$last_name = "Vazquez";
+
+echo $first_name;
+echo " ";
+echo $last_name;
+
+$startyear = 2016;
+$actualyear = 2020;
+echo "<h1>Year: $actualyear </h1>";
+echo "Start year: ".$startyear;
+
+//arimetric operators
+
+$spendedyears = $actualyear - $startyear;
+echo "<h3> Llevo: $spendedyears anos en mi carrera </h3> ";
+
+$totalmaterias = 50;
+$materiastomadas = 33;
+$cuatriactual = 7;
+$cuatrimestres = 10;
+$cuatristomados = 7;
+$restante = $totalmaterias - $materiastomadas;
+
+echo "Me faltan $restante materias por tomar";
+//   /n salto de linea al final y dentro de comillas
+echo "<br>";
+//suma mult y division
+
+if ($spendedyears > 5) {
+    echo "hay la llevas";
+} else {
+    echo "no la llevas";
+    echo "<br>";
+}
+
+?>
+
+<a href="http"></a>
+
+<?
+
+// camel case
+$firstName = "";
+
+//lowercase
+$firstname = "";
+
+?>
